269fae9aef767341462809b5160633847a52ad0c
47gfx <117771075+4700gfx@users.noreply.github.com>
2026-04-13 20:03:47 -0400

    Adding More Functionality with State Management

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,107 @@
-import { useEffect, useState } from "react";
+import { useState, useReducer, useEffect } from "react";
 import UploadForm from "./components/UploadForm";
 import Navbar from './components/Navbar';
 import Card from './components/Card';
 
-function App() {
-  const photos = [
-    "https://images.unsplash.com/photo-1492724441997-5dc865305da7?auto=format&fit=crop&w=800&q=80",
-    "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?auto=format&fit=crop&w=800&q=80",
-    "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?auto=format&fit=crop&w=800&q=80",
-    "https://images.unsplash.com/photo-1498050108023-c5249f4df085?auto=format&fit=crop&w=800&q=80",
-    "https://images.unsplash.com/photo-1521737604893-d14cc237f11d?auto=format&fit=crop&w=800&q=80",
-    "https://images.unsplash.com/photo-1519389950473-47ba0277781c?auto=format&fit=crop&w=800&q=80",
-    "https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?auto=format&fit=crop&w=800&q=80",
-    "https://images.unsplash.com/photo-1497215728101-856f4ea42174?auto=format&fit=crop&w=800&q=80"
-  ];
+const photos = [
+  "https://images.unsplash.com/photo-1492724441997-5dc865305da7?auto=format&fit=crop&w=800&q=80",
+  "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?auto=format&fit=crop&w=800&q=80",
+  "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?auto=format&fit=crop&w=800&q=80",
+  "https://images.unsplash.com/photo-1498050108023-c5249f4df085?auto=format&fit=crop&w=800&q=80",
+  "https://images.unsplash.com/photo-1521737604893-d14cc237f11d?auto=format&fit=crop&w=800&q=80",
+  "https://images.unsplash.com/photo-1519389950473-47ba0277781c?auto=format&fit=crop&w=800&q=80",
+  "https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?auto=format&fit=crop&w=800&q=80",
+  "https://images.unsplash.com/photo-1497215728101-856f4ea42174?auto=format&fit=crop&w=800&q=80"
+];
 
-  const [count, setCount] = useState("");
-  const [inputs, setInputs] = useState({ title: "", file: null, path: null });
-  const [items, setItems] = useState(photos);
-  const [isCollapsed, collapse] = useState(false);
+const intialState = {
+  items: photos, 
+  count: photos.length,
+  inputs: { title: "", file: null, path: null },
+  isCollapsed: false 
+}
 
-  const toggle = () => collapse(!isCollapsed);
+  const handleOnChange = (state, e) => {
 
-  const handleOnChange = (e) => {
+    //Checks to See if The Target is The File 
     if (e.target.name === 'file') {
-      setInputs({
-        ...inputs, 
+
+      //Set the State to Include the Previous Picture with New File 
+      return {
+        ...state.inputs, 
         file: e.target.files[0], 
         path: URL.createObjectURL(e.target.files[0])
-      });
+      };
     } else {
-      setInputs({ ...inputs, title: e.target.value });
+      //Sets the Title to the Value 
+      return { ...state.inputs, title: e.target.value };
     }
   };
 
-  const handleOnSubmit = (e) => {
-    if (e) e.preventDefault();
-    
-    if (inputs.path) {
-      setItems([inputs.path, ...items]);
-      setInputs({ title: "", file: null, path: null });
-      collapse(false);
-    }
-  };
+
+function reducer(state, action){
+  switch(action.type){
+    case 'setItem':
+      return{
+        ...state,
+        items: [state.inputs, ...state.items]
+      }
+    case "setInputs":
+      return{
+        ...state,
+        inputs: handleOnChange(action.payload.value)
+      }
+
+    case 'collaspe':
+      return{
+        ...state, 
+        isCollapsed :action.payload.bool
+      }
+    default : return state
+  }
+}
+
+
+function App() {
+
+  const [state, dispatch] = useReducer(reducer, intialState)
+  const [count, setCount] = useState()
+  const toggle = (bool) => dispatch({type: 'collapse', payload: {bool} })
+
+  useEffect(() =>{
+    console.log(state)
+  }, [state])
+
 
   useEffect(() => {
-    // Use parentheses, not backticks!
-    setCount(`You Have ${items.length} Image${items.length > 1 ? 's' : ''}`);
-  }, [items]);
+    setCount(`You have ${state.items.length} image${state.items.length > 1 ? 's' : ''}`)
+  }, [state])
+
+  const handleOnChange = (state, e) => dispatch({type: 'setInput', payload:{value: e}})
+
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault()
+    dispatch({type: 'setItem'})
+    toggle(!state.isCollapsed)
+  };
+
+
 
   return (
     <div className="container text-center">
       <Navbar />
       <div className="container text-center mt-5">
-        <button className="btn btn-success float" onClick={toggle}>
-          {isCollapsed ? "CLOSE" : "+ ADD AN IMAGE"}
+        <button className="btn btn-success float" onClick={() => toggle(!state.isCollapsed)}>
+          {state.isCollapsed ? "CLOSE" : "+ ADD AN IMAGE"}
         </button>
         <UploadForm 
-          isVisible={isCollapsed}
+          //Passing Handle Functions and State as Props 
+          inputs={state.inputs}
+          isVisible={state.isCollapsed}
           onChange={handleOnChange}
           onSubmit={handleOnSubmit}
-          value={inputs}
+          value={state.inputs}
         />
         <h1 className="mt-5 mb-4">Gallery</h1>
         
@@ -68,7 +109,7 @@ function App() {
         <p className="mb-3 text-muted">{count}</p>
         
         <div className="row justify-content-center g-4">
-          {items.map((photo, index) => (
+          {state.items.map((photo, index) => (
             <Card key={`${photo}-${index}`} image={photo} />
           ))}
         </div>
--- src/components/UploadForm.jsx
+++ src/components/UploadForm.jsx
@@ -1,8 +1,36 @@
-const UploadForm = ({ isVisible, onChange, onSubmit }) => {
+import { useMemo } from "react";  
+
+const Preview = ({path}) => {
+  return(
+    path && <div
+      className="rounded p-1"
+      style={{
+        width: "30%",
+        height: "200px",
+        marginTop: "20px",
+        marginRight: "20px",
+        backgroundImage: `url(${path})`,
+        backgroundSize: "cover"
+      }}
+    ></div>
+  )
+}
+
+
+const UploadForm = ({ inputs, isVisible, onChange, onSubmit }) => {
+
+  //Disables Elements If The Form is Not Complete
+  const isDisabled = useMemo(() => {
+    return !!Object.values(inputs).some(input => !input)
+  }, [inputs])
+  
+  
   return (
+    //Returns When Visible 
     isVisible &&     <>
       <p className="display-6 text-center mb-3">Upload Stock Image</p>
       <div className="mb-5 d-flex align-items-center justify-content-center">
+        <Preview {...inputs} />
         <form 
           className="mb-2" 
           style={{ textAlign: "left" }}        
@@ -29,6 +57,7 @@ const UploadForm = ({ isVisible, onChange, onSubmit }) => {
           <button
             type="submit"
             className="btn btn-success float-end"
+            disabled={isDisabled}
           >
             Save changes
           </button>
